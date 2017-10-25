45fdc4da993a0fc713e4d2983006cff4a8e8d59c
Digitroinc <craigbett@gmail.com>
2017-10-25 17:03:53 -0400

    Opps Rip

--- gamemode/maps/ph_underwataaa.lua
+++ gamemode/maps/ph_underwataaa.lua
@@ -1,14 +1,14 @@
 if SERVER then
 	hook.Add('Initialize', 'Initialize:LPSMap:Underwataaa', function()
-        for id, loadout in pairs(GM.loadouts) do
-            if (GM.loadouts[id]['weapon_smg1']) then
-                GM.loadouts[id]['weapon_smg1_underwata'] = table.Copy(GM.loadouts[id]['weapon_smg1'])
-                GM.loadouts[id]['weapon_smg1'] = nil
+        for id, loadout in pairs(GAMEMODE.loadouts) do
+            if (GAMEMODE.loadouts[id]['weapon_smg1']) then
+                GAMEMODE.loadouts[id]['weapon_smg1_underwata'] = table.Copy(GM.loadouts[id]['weapon_smg1'])
+                GAMEMODE.loadouts[id]['weapon_smg1'] = nil
             end
 
-            if (GM.loadouts[id]['weapon_ar2']) then
-                GM.loadouts[id]['weapon_ar2_underwata'] = table.Copy(GM.loadouts[id]['weapon_ar2'])
-                GM.loadouts[id]['weapon_ar2'] = nil
+            if (GAMEMODE.loadouts[id]['weapon_ar2']) then
+                GAMEMODE.loadouts[id]['weapon_ar2_underwata'] = table.Copy(GAMEMODE.loadouts[id]['weapon_ar2'])
+                GAMEMODE.loadouts[id]['weapon_ar2'] = nil
             end
         end
 	end)
