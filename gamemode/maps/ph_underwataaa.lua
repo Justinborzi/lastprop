a3e725552f6690fe6b248685374861da1fb60687
Digitroinc <craigbett@gmail.com>
2017-10-25 17:01:51 -0400

    Underwataaa Bugfix?

--- gamemode/maps/ph_underwataaa.lua
+++ gamemode/maps/ph_underwataaa.lua
@@ -1,5 +1,5 @@
 if SERVER then
-	hook.Add('PostGamemodeLoaded', 'PostGamemodeLoaded:LPSMap:Underwataaa', function()
+	hook.Add('Initialize', 'Initialize:LPSMap:Underwataaa', function()
         for id, loadout in pairs(GM.loadouts) do
             if (GM.loadouts[id]['weapon_smg1']) then
                 GM.loadouts[id]['weapon_smg1_underwata'] = table.Copy(GM.loadouts[id]['weapon_smg1'])
